dc87e232af64f54dfc398e949ead1c48592c0ff3
Carl <carlh3h@yahoo.co.id>
2019-10-28 15:35:54 +0700

    fix variable name

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function toRoman(num){
-  var res = ''
+  var result = ''
   var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
   var arabic = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
   for (var i = arabic.length - 1; i >= 0; i -= 1){
     while (num >= arabic[i]){
       num -= arabic[i]
-      res += (roman[i])
+      result += (roman[i])
     }
   }
-  return res
+  return result
 }
 
 console.log(toRoman(4))
