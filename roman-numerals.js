ba8611d78765af07aec93cf714f620518e2cb2a8
Michael <michael.wenceslaus@gmail.com>
2019-10-28 18:34:13 +0700

    roman-numerals

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,5 +1,4 @@
 function toRoman(input) {
-    const iStr = input.toString();
     const tabel = [
         ['I', 1], ['IV', 4], ['V', 5], ['IX', 9], ['X', 10],
         ['XL', 40], ['L', 50], ['XC', 90], ['C', 100], ['CD', 400],
