1b21aa3184d08afd3f38757f01dba9b0b717159f
Trayand <patradyn@gmail.com>
2019-10-28 15:19:09 +0700

    baru

--- roma.js
+++ roma.js
@@ -34,3 +34,43 @@ console.log(toRoman(1));
 console.log(toRoman(11));
 console.log(toRoman(111));
 console.log(toRoman(1111));
+
+// function recursive(num){
+//     var str = num.toString()
+//     if(str.length== 1){
+//         return [str[0]]
+//     }else{
+//         var check = ''
+//         for(let i=0;i<str.length-1;i++){
+//             check+= '0'
+//         }
+//         return [str[0]+check].concat(recursive(str.slice(1)))
+//     }
+// }
+
+// function toRoman (num) {
+//     var arr = recursive(num)
+//     var result = ''
+//     var count = 0
+//     for(let j = 0; j < arr.length; j++){
+//         for(key in alfa){
+//             while(arr[j]-alfa[key]>=0){
+//                 result+=key
+//                 arr[j] -= alfa[key]
+//                 count++
+//                 if(count > 3){
+//                     count = 0
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+
+//     return result
+// }
+
+// console.log(toRoman(1));
+// console.log(toRoman(11));
+// console.log(toRoman(111));
+// console.log(toRoman(1111));
+
